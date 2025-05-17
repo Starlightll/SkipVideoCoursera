c76418230cb1358f23ef4a0d5b0c4e5067a4d4be
NGUYEN HOANG LONG <145125551+Starlightll@users.noreply.github.com>
2025-05-17 20:08:55 +0700

    Enhance video skipping functionality and add input range for skip percentage

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
+
+
 document.getElementById("myButton").addEventListener("click", function () {
+// Get the skipPercentage value from the popup
+  const skipPercentValue = document.getElementById("skipPercentage").value;
+
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     if (tabs && tabs.length > 0) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        function: () => {
-          function SkipVideo() {
+        args: [skipPercentValue],
+        function: (skipPercentValue) => {
+          function SkipVideo(skipPercent) {
+            if(skipPercent == "") {
+              skipPercent = 0;
+            }else if(skipPercent > 100) {
+              skipPercent = 100;
+            }else if(skipPercent < 0) {
+              skipPercent = 0;
+            }else{
+              skipPercent = parseInt(skipPercent);
+            }
             var durationDisplay = document.getElementsByClassName("duration-display")[0];
 
             var currentVideoTime = document.getElementsByClassName("current-time-display")[0];
@@ -19,7 +34,7 @@ document.getElementById("myButton").addEventListener("click", function () {
               var currentTime = parseTime(currentTimeText);
 
               var remainingTime = duration - currentTime;
-              for (i = 0; i < remainingTime - 0.1*remainingTime; i += 10) {
+              for (i = 0; i < remainingTime -  (1 - skipPercent/100)*remainingTime; i += 10) {
                 // console.log(i);
                 let buttons = document.getElementsByClassName("css-4a32g3");
                 if (buttons.length > 1) {
@@ -28,8 +43,8 @@ document.getElementById("myButton").addEventListener("click", function () {
                   console.warn("Skip button not found!");
                 }
               }
-              playButton.click();
-              playButton.click();
+              // playButton.click();
+              // playButton.click();
               console.log("Remaining time: " + remainingTime + " seconds");
               // Thực hiện hành động bỏ qua video ở đây, ví dụ:
               // video.currentTime = duration - 5; // Tua đến 5 giây trước khi hết video
@@ -54,7 +69,7 @@ document.getElementById("myButton").addEventListener("click", function () {
             ).singleNodeValue;
           }
 
-          SkipVideo(); // Gọi SkipVideo() sau khi đã định nghĩa các hàm
+          SkipVideo(skipPercentValue); // Gọi SkipVideo() sau khi đã định nghĩa các hàm
         },
       });
     } else {
